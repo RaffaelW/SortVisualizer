ea517762dbbef2bf71417d7d4f04f0f1683f2884
Raffa-W <rfflwlf@gmail.com>
2023-09-30 17:04:34 +0200

    changed comment

--- app/src/main/java/com/raffascript/sortvisualizer/presentation/MainActivity.kt
+++ app/src/main/java/com/raffascript/sortvisualizer/presentation/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : ComponentActivity() {
         }
     }
     /*
+    * uploaded project on github
      * To do:
      * measure time (maybe only with delay 0)
      * count number of array accesses and comparisons
