561967a8f2580a58c73e8d5d8dafc902ef9a9703
RaffaelW <rfflwlf@gmail.com>
2023-10-01 19:42:48 +0200

    added layout for landscape

--- app/src/main/java/com/raffascript/sortvisualizer/presentation/visualizer/VisualizerScreen.kt
+++ app/src/main/java/com/raffascript/sortvisualizer/presentation/visualizer/VisualizerScreen.kt
@@ -1,8 +1,9 @@
 package com.raffascript.sortvisualizer.presentation.visualizer
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.PlayArrow
@@ -10,8 +11,11 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.raffascript.sortvisualizer.R
@@ -21,34 +25,74 @@ import com.raffascript.sortvisualizer.presentation.visualizer.chart.Chart
 
 @Composable
 fun VisualizerScreen(state: VisualizerState, onEvent: (VisualizerUiEvent) -> Unit) {
-    Column(modifier = Modifier.fillMaxSize()) {
+    val configuration = LocalConfiguration.current
 
-        if (state.isBottomSheetShown) {
-            BottomSheet(
-                delay = state.sliderDelay,
-                inputListSize = state.inputListSize,
-                isListSizeInputValid = state.isInputListSizeValid,
-                onEvent = onEvent,
-                onDismiss = { onEvent(VisualizerUiEvent.HideBottomSheet) }
-            )
-        }
-
-        Chart(
-            list = state.sortingList,
-            highlights = state.highlights,
-            modifier = Modifier.fillMaxWidth().height(300.dp)
+    if (state.isBottomSheetShown) {
+        BottomSheet(
+            delay = state.sliderDelay,
+            inputListSize = state.inputListSize,
+            isListSizeInputValid = state.isInputListSizeValid,
+            onEvent = onEvent,
+            onDismiss = { onEvent(VisualizerUiEvent.HideBottomSheet) }
         )
-
-        Surface(modifier = Modifier.fillMaxWidth(), color = MaterialTheme.colorScheme.secondary) {
-            ProgressSurface(state.comparisonCount, state.arrayAccessCount)
-        }
-
-        Surface(modifier = Modifier.fillMaxWidth().weight(2f), color = MaterialTheme.colorScheme.surface) {
-            AlgorithmDataSurface(state.algorithmName)
-        }
-
-        BottomBar(state.algorithmState, onEvent = onEvent)
     }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        when (configuration.orientation) {
+            Configuration.ORIENTATION_LANDSCAPE -> {
+                ContentLandscape(state = state, onEvent = onEvent)
+            }
+            else -> {
+                ContentPortrait(state = state, onEvent = onEvent)
+            }
+        }
+    }
+}
+
+@Composable
+fun ColumnScope.ContentPortrait(state: VisualizerState, onEvent: (VisualizerUiEvent) -> Unit) {
+    Chart(
+        list = state.sortingList,
+        highlights = state.highlights,
+        modifier = Modifier.fillMaxWidth().height(300.dp)
+    )
+
+    Surface(modifier = Modifier.fillMaxWidth(), color = MaterialTheme.colorScheme.secondary) {
+        ProgressSurface(state.comparisonCount, state.arrayAccessCount)
+    }
+
+    Surface(modifier = Modifier.fillMaxWidth().weight(1f), color = MaterialTheme.colorScheme.surface) {
+        AlgorithmDataSurface(
+            modifier = Modifier.verticalScroll(rememberScrollState()),
+            algorithmName = state.algorithmName
+        )
+    }
+
+    BottomBar(state.algorithmName, false, state.algorithmState, onEvent = onEvent)
+}
+
+@Composable
+fun ColumnScope.ContentLandscape(state: VisualizerState, onEvent: (VisualizerUiEvent) -> Unit) {
+    // use LazyColumn to get access to Modifier.fillParentMaxHeight()
+    LazyColumn(modifier = Modifier.weight(1f)) {
+        item {
+            Chart(
+                list = state.sortingList,
+                highlights = state.highlights,
+                modifier = Modifier.fillMaxWidth().fillParentMaxHeight()
+            )
+
+            Surface(modifier = Modifier.fillMaxWidth(), color = MaterialTheme.colorScheme.secondary) {
+                ProgressSurface(state.comparisonCount, state.arrayAccessCount)
+            }
+
+            Surface(modifier = Modifier.fillMaxWidth(), color = MaterialTheme.colorScheme.surface) {
+                AlgorithmDataSurface(algorithmName = state.algorithmName)
+            }
+        }
+    }
+
+    BottomBar(state.algorithmName, true, state.algorithmState, onEvent = onEvent)
 }
 
 @Composable
@@ -63,14 +107,19 @@ fun ProgressSurface(comparisonCount: Long, arrayAccessCount: Long) {
 }
 
 @Composable
-fun AlgorithmDataSurface(algorithmName: String) {
-    Column(modifier = Modifier.fillMaxSize().padding(16.dp).verticalScroll(rememberScrollState())) {
+fun AlgorithmDataSurface(modifier: Modifier = Modifier, algorithmName: String) {
+    Column(modifier = modifier.fillMaxWidth().padding(16.dp)) {
         Text(text = algorithmName, style = MaterialTheme.typography.titleLarge)
     }
 }
 
 @Composable
-fun BottomBar(algorithmState: AlgorithmState, onEvent: (VisualizerUiEvent) -> Unit) {
+fun BottomBar(
+    algorithmName: String,
+    showName: Boolean,
+    algorithmState: AlgorithmState,
+    onEvent: (VisualizerUiEvent) -> Unit
+) {
     BottomAppBar(
         containerColor = MaterialTheme.colorScheme.surface,
         tonalElevation = 10.dp,
@@ -87,10 +136,18 @@ fun BottomBar(algorithmState: AlgorithmState, onEvent: (VisualizerUiEvent) -> Un
                     contentDescription = stringResource(R.string.replay)
                 )
             }
+
+            if (showName) {
+                Text(
+                    text = algorithmName,
+                    modifier = Modifier.padding(horizontal = 8.dp).weight(2f),
+                    style = MaterialTheme.typography.titleMedium,
+                    textAlign = TextAlign.Center
+                )
+            }
         },
         floatingActionButton = {
             FloatingActionButton(
-                shape = CircleShape,
                 containerColor = MaterialTheme.colorScheme.primary,
                 onClick = {
                     when (algorithmState) {
@@ -114,7 +171,8 @@ fun BottomBar(algorithmState: AlgorithmState, onEvent: (VisualizerUiEvent) -> Un
     )
 }
 
-@Preview
+@Preview(name = "Portrait")
+@Preview(name = "Landscape", device = Devices.AUTOMOTIVE_1024p, widthDp = 720, heightDp = 360)
 @Composable
 fun VisualizerPreview() {
     AlgorithmsVisualizerTheme(darkTheme = true) {
