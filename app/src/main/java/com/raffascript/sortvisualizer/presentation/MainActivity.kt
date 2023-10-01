3bedcd068e4b2f1bad9903da8e809472d8069c97
RaffaelW <rfflwlf@gmail.com>
2023-10-01 15:14:07 +0200

    moved todo list from MainActivity to  todo.md

--- app/src/main/java/com/raffascript/sortvisualizer/presentation/MainActivity.kt
+++ app/src/main/java/com/raffascript/sortvisualizer/presentation/MainActivity.kt
@@ -20,21 +20,4 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-    /*
-     * heapsort update progress
-     * To do:
-     * measure time (maybe only with delay 0)
-     * Typo
-     * full screen
-     * show algorithm best/worst/average time complexity, description ...
-     * sounds
-     * launcher icon
-     * Implement Algorithms:
-     * - MergeSort
-     * - QuickSort
-     * - RadixSort
-     * - HeapSort
-     * - CountingSort
-     * (- TimSort)
-     */
 }
---
+++ todo.md
@@ -0,0 +1,14 @@
+# Todo
+
++ measure time (maybe only with delay 0)
++ Typo
++ full screen
++ show algorithm best/worst/average time complexity, description ...
++ sounds
++ launcher icon
++ Implement Algorithms:
+    + MergeSort
+    + QuickSort
+    + RadixSort
+    + CountingSort
+    + (TimSort)
