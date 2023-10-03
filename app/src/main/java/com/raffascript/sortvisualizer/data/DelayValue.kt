a2ebe001473429f1917e38aa0dd0de4d740c8e36
RaffaelW <rfflwlf@gmail.com>
2023-10-03 18:19:26 +0200

    selected better delay values

--- app/src/main/java/com/raffascript/sortvisualizer/data/DelayValue.kt
+++ app/src/main/java/com/raffascript/sortvisualizer/data/DelayValue.kt
@@ -6,13 +6,14 @@ import kotlin.time.Duration.Companion.milliseconds
 enum class DelayValue(val millis: Float) {
     DELAY_0(0f),
     DELAY_1(1f),
+    DELAY_2(2f),
+    DELAY_3(3f),
+    DELAY_4(4f),
     DELAY_5(5f),
     DELAY_10(10f),
     DELAY_50(50f),
     DELAY_100(100f),
-    DELAY_300(300f),
     DELAY_500(500f),
-    DELAY_700(700f),
     DELAY_1000(1000f),
     DELAY_1500(1500f),
     DELAY_2000(2000f);
