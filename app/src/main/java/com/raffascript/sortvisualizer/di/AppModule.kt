13b057d4b06a7be431caa10d12ebe331acdcaa6c
RaffaelW <rfflwlf@gmail.com>
2023-10-03 20:45:44 +0200

    added UserPreferencesRepository and UserPreferencesDataSource to dependency injection

--- app/src/main/java/com/raffascript/sortvisualizer/di/AppModule.kt
+++ app/src/main/java/com/raffascript/sortvisualizer/di/AppModule.kt
@@ -2,13 +2,23 @@ package com.raffascript.sortvisualizer.di
 
 import android.content.Context
 import com.raffascript.sortvisualizer.data.AlgorithmRegister
+import com.raffascript.sortvisualizer.data.preferences.UserPreferencesDataSource
+import com.raffascript.sortvisualizer.data.preferences.UserPreferencesRepository
 
 interface AppModule {
     val algorithmRegister: AlgorithmRegister
+    val userPreferencesRepository: UserPreferencesRepository
+    val userPreferencesDataSource: UserPreferencesDataSource
 }
 
 class AppModuleImpl(private val context: Context) : AppModule {
     override val algorithmRegister: AlgorithmRegister by lazy {
         AlgorithmRegister()
+    }
+    override val userPreferencesRepository: UserPreferencesRepository by lazy {
+        UserPreferencesRepository(userPreferencesDataSource)
+    }
+    override val userPreferencesDataSource: UserPreferencesDataSource by lazy {
+        UserPreferencesDataSource(context)
     }
 }
