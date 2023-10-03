95a557e89a63da11e9869e30cbc2fd9acaedabb7
RaffaelW <rfflwlf@gmail.com>
2023-10-03 20:42:11 +0200

    added getter and setter for UserPreferences

--- app/src/main/java/com/raffascript/sortvisualizer/data/preferences/UserPreferencesRepository.kt
+++ app/src/main/java/com/raffascript/sortvisualizer/data/preferences/UserPreferencesRepository.kt
@@ -1,6 +1,23 @@
 package com.raffascript.sortvisualizer.data.preferences
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+
 class UserPreferencesRepository(
-    private val userPreferencesDataStore: UserPreferencesDataStore
+    private val preferencesDataSource: UserPreferencesDataStore
 ) {
+
+    fun getUserPreferencesFlow(): Flow<UserPreferences> {
+        return combine(preferencesDataSource.delayFlow, preferencesDataSource.listSizeFlow) { delay, listSize ->
+            UserPreferences(delay, listSize)
+        }
+    }
+
+    suspend fun saveUserPreferences(userPreferences: UserPreferences) {
+        val (delay, listSize) = userPreferences
+        preferencesDataSource.apply {
+            setDelay(delay)
+            setListSize(listSize)
+        }
+    }
 }
