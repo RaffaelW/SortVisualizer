991d593be9c7d0f38429b02e976c9b58d5d57e8c
RaffaelW <rfflwlf@gmail.com>
2023-10-03 20:35:12 +0200

    moved listSize default value to UserPreferencesDataStore

--- app/src/main/java/com/raffascript/sortvisualizer/presentation/visualizer/BottomSheet.kt
+++ app/src/main/java/com/raffascript/sortvisualizer/presentation/visualizer/BottomSheet.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.raffascript.sortvisualizer.R
 import com.raffascript.sortvisualizer.data.DelayValue
+import com.raffascript.sortvisualizer.data.preferences.UserPreferencesDataStore
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -82,7 +83,7 @@ fun BottomSheet(
                 IconButton(
                     onClick = {
                         onEvent(VisualizerUiEvent.ChangeDelay(DelayValue.default))
-                        onEvent(VisualizerUiEvent.ChangeListSizeInput("${VisualizerViewModel.DEFAULT_LIST_SIZE}"))
+                        onEvent(VisualizerUiEvent.ChangeListSizeInput("${UserPreferencesDataStore.DEFAULT_LIST_SIZE}"))
                     }
                 ) {
                     Icon(imageVector = Icons.Rounded.Refresh, contentDescription = stringResource(R.string.reset))
