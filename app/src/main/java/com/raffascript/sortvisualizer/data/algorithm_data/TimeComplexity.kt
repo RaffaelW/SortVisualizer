c098c8e6bee0b0724131521903f9dadc1418669a
RaffaelW <rfflwlf@gmail.com>
2023-10-08 22:17:48 +0200

    replaced html entities with characters

--- app/src/main/java/com/raffascript/sortvisualizer/data/algorithm_data/TimeComplexity.kt
+++ app/src/main/java/com/raffascript/sortvisualizer/data/algorithm_data/TimeComplexity.kt
@@ -1,10 +1,16 @@
 package com.raffascript.sortvisualizer.data.algorithm_data
 
+import android.text.Html
+
 enum class TimeComplexity(val abbreviation: String) {
     CONSTANT("O(1)"),
     LOGARITHMIC("O(log n)"),
     LINEAR("O(n)"),
     QUASILINEAR("O(n log n)"),
-    QUADRATIC("O(n^2)"),
-    INFINITE("&infin;");
+    QUADRATIC("O(n²)"),
+    INFINITE("∞");
+
+    init {
+        Html.fromHtml("fdf", 0)
+    }
 }
