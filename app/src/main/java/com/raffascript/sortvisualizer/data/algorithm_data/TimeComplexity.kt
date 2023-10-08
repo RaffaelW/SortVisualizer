b4b3a43b3805a8c6f37d4b19c183f3e87d44c557
RaffaelW <rfflwlf@gmail.com>
2023-10-08 19:28:57 +0200

    added abbreviation to time complexity

--- app/src/main/java/com/raffascript/sortvisualizer/data/algorithm_data/TimeComplexity.kt
+++ app/src/main/java/com/raffascript/sortvisualizer/data/algorithm_data/TimeComplexity.kt
@@ -1,10 +1,10 @@
 package com.raffascript.sortvisualizer.data.algorithm_data
 
-enum class TimeComplexity {
-    CONSTANT,
-    LOGARITHMIC,
-    LINEAR,
-    QUASILINEAR,
-    QUADRATIC,
-    INFINITE;
+enum class TimeComplexity(val abbreviation: String) {
+    CONSTANT("O(1)"),
+    LOGARITHMIC("O(log n)"),
+    LINEAR("O(n)"),
+    QUASILINEAR("O(n log n)"),
+    QUADRATIC("O(n^2)"),
+    INFINITE("&infin;");
 }
