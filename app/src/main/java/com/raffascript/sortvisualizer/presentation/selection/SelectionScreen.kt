3dc33c83819dab61c29c32bb79bb54085d4f9873
RaffaelW <rfflwlf@gmail.com>
2023-10-09 19:58:17 +0200

    modified the SelectionScreen so the screen is always in portrait mode

--- app/src/main/java/com/raffascript/sortvisualizer/presentation/selection/SelectionScreen.kt
+++ app/src/main/java/com/raffascript/sortvisualizer/presentation/selection/SelectionScreen.kt
@@ -1,5 +1,8 @@
 package com.raffascript.sortvisualizer.presentation.selection
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.pm.ActivityInfo
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,7 +13,9 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.raffascript.sortvisualizer.data.algorithm_data.AlgorithmData
@@ -20,6 +25,12 @@ import com.raffascript.sortvisualizer.presentation.theme.AlgorithmsVisualizerThe
 
 @Composable
 fun SelectionScreen(state: SelectionState, navigateToVisualizer: (Int) -> Unit) {
+    val activity = LocalContext.current as Activity
+    LaunchedEffect(key1 = Unit) {
+        @SuppressLint("SourceLockedOrientationActivity")
+        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    }
+
     LazyColumn(modifier = Modifier.fillMaxSize()) {
         itemsIndexed(items = state.algorithmData, key = { _, item -> item.id }) { index, item ->
             Row(item, onClick = { navigateToVisualizer(item.id) })
