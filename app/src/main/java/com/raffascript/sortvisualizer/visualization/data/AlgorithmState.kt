88f62cf79f4dbe0540b495f17c637e87537b5e40
RaffaelW <rfflwlf@gmail.com>
2023-10-31 15:28:58 +0100

    added state diagram

--- app/src/main/java/com/raffascript/sortvisualizer/visualization/data/AlgorithmState.kt
+++ app/src/main/java/com/raffascript/sortvisualizer/visualization/data/AlgorithmState.kt
@@ -3,3 +3,18 @@ package com.raffascript.sortvisualizer.visualization.data
 enum class AlgorithmState {
     UNINITIALIZED, READY, RUNNING, PAUSED, FINISHED;
 }
+
+/*
+ *                 Uninitialized
+ *                      ||
+ *                      \/
+ *                     Ready <---------+
+ *                      ||             | Restart
+ *                      \/             |
+ *         +--------> Running ---------|
+ *  Resume |          ||   ||          |
+ *         |          \/   \/          |
+ *         +----- Paused  Finished ----|
+ *                   |                 |
+ *                   +-----------------+
+ */
