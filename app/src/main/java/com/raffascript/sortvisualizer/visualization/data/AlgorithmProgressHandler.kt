4abfee241701b81b522a89f4d391fff0fc30e1f2
RaffaelW <rfflwlf@gmail.com>
2023-10-31 23:04:03 +0100

    removed an interface that is no longer used

--- app/src/main/java/com/raffascript/sortvisualizer/visualization/data/AlgorithmProgressHandler.kt
+++
@@ -1,8 +0,0 @@
-package com.raffascript.sortvisualizer.visualization.data
-
-interface AlgorithmProgressHandler {
-
-    suspend fun onStep(vararg highlights: Highlight)
-
-    suspend fun onFinish()
-}
