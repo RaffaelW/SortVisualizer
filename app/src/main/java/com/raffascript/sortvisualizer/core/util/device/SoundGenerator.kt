455e378091d0d21f6ab90b5663f4500959dca840
RaffaelW <rfflwlf@gmail.com>
2023-10-31 23:11:31 +0100

    cleaned the code up

--- app/src/main/java/com/raffascript/sortvisualizer/core/util/device/SoundGenerator.kt
+++ app/src/main/java/com/raffascript/sortvisualizer/core/util/device/SoundGenerator.kt
@@ -4,7 +4,6 @@ import android.media.AudioAttributes
 import android.media.AudioFormat
 import android.media.AudioManager
 import android.media.AudioTrack
-import android.util.Log
 import kotlin.math.sin
 import kotlin.time.Duration
 
@@ -20,10 +19,6 @@ class SoundGenerator(override val soundDuration: Duration) : SoundPlayer {
     private val sampleRate = 8000
     private val numSamples = ((duration / 1000F) * sampleRate).toInt()
 
-    init {
-        Log.d("SoundGenerator", "duration: $duration ms, numSamples: $numSamples")
-    }
-
     private val sample = DoubleArray(numSamples)
     private val generatedSound = ByteArray(2 * numSamples)
 
@@ -34,13 +29,11 @@ class SoundGenerator(override val soundDuration: Duration) : SoundPlayer {
 
     private val generator = Thread {
 //        Thread.currentThread().priority = Thread.MIN_PRIORITY
-        Log.d("SoundGenerator", "SoundGenerator thread started, duration: $duration ms")
         audioTrack.play()
         while (isRunning) {
             frequency?.let { freq ->
                 this.frequency = null
                 generateTone(freq)
-                Log.d("SoundGenerator", "Playing tone, frequency: $freq")
                 audioTrack.write(generatedSound, 0, generatedSound.size)
             }
         }
@@ -49,7 +42,6 @@ class SoundGenerator(override val soundDuration: Duration) : SoundPlayer {
     }
 
     override fun start() {
-        Log.d("SoundGenerator", "Starting sound generator")
         isRunning = true
         generator.start()
     }
@@ -61,7 +53,6 @@ class SoundGenerator(override val soundDuration: Duration) : SoundPlayer {
     override fun play(frequency: Float) {
         audioTrack.flush()
         this.frequency = frequency
-        Log.d("SoundGenerator", "Adding tone")
     }
 
     private fun buildAudioTrack(): AudioTrack {
@@ -79,7 +70,6 @@ class SoundGenerator(override val soundDuration: Duration) : SoundPlayer {
                     .setLegacyStreamType(AudioManager.STREAM_MUSIC)
                     .build()
             )
-//            setBufferSizeInBytes(generatedSound.size)
         }.build()
     }
 
@@ -101,70 +91,3 @@ class SoundGenerator(override val soundDuration: Duration) : SoundPlayer {
         }
     }
 }
-
-/*
-class SoundGenerator(private val activity: Activity, soundDuration: Duration) {
-
-    private val duration = soundDuration.inWholeMilliseconds
-    private val sampleRate = 8000
-    private val numSamples = ((duration / 1000.0) * sampleRate).toInt()
-
-    private val sample = DoubleArray(numSamples)
-    private val generatedSound = ByteArray(2 * numSamples)
-
-    private val audioTrack = buildAudioTrack()
-
-    private fun buildAudioTrack(): AudioTrack {
-        return AudioTrack.Builder().apply {
-            setTransferMode(AudioTrack.MODE_STATIC)
-            setAudioFormat(
-                AudioFormat.Builder()
-                    .setSampleRate(sampleRate)
-                    .setChannelMask(AudioFormat.CHANNEL_OUT_MONO)
-                    .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
-                    .build()
-            )
-            setAudioAttributes(
-                AudioAttributes.Builder()
-                    .setLegacyStreamType(AudioManager.STREAM_MUSIC)
-                    .build()
-            )
-            setBufferSizeInBytes(generatedSound.size)
-            setTransferMode(AudioTrack.MODE_STATIC)
-        }.build()
-    }
-    fun play(frequency: Float) {
-        // Use a new tread as this can take a while
-        val thread = Thread {
-            generateTone(frequency)
-            activity.runOnUiThread {
-                playSound()
-            }
-        }
-        thread.start()
-    }
-
-    private fun generateTone(frequency: Float) {
-        // fill out the array
-        for (i in 0 until numSamples) {
-            sample[i] = sin(2 * Math.PI * i / (sampleRate / frequency))
-        }
-
-        // convert to 16 bit pcm sound array
-        // assumes the sample buffer is normalised.
-        var index = 0
-        for (dVal in sample) {
-            // scale to maximum amplitude
-            val value = (dVal * 32767).toInt().toShort()
-            // in 16 bit wav PCM, first byte is the low order byte
-            generatedSound[index++] = (value.toInt() and 0x00ff).toByte()
-            generatedSound[index++] = (value.toInt() and 0xff00 ushr 8).toByte()
-        }
-    }
-
-    private fun playSound() {
-        audioTrack.write(generatedSound, 0, generatedSound.size)
-        audioTrack.play()
-    }
-}
-*/
